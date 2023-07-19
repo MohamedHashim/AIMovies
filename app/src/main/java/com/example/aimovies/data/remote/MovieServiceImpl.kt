e78eba612d42dac669274384e8d4a4c5bcb0f24e
AElkhami <2000088331@hexaware.com>
2023-07-19 18:01:51 +0200

    *Moving Auth_Key to build config.

--- app/src/main/java/com/example/aimovies/data/remote/MovieServiceImpl.kt
+++ app/src/main/java/com/example/aimovies/data/remote/MovieServiceImpl.kt
@@ -1,5 +1,6 @@
 package com.example.aimovies.data.remote
 
+import com.example.aimovies.BuildConfig
 import com.example.aimovies.data.remote.dto.DiscoverMovieResponse
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
@@ -15,7 +16,7 @@ class MovieServiceImpl(private val client: HttpClient) : MovieService {
         return try {
             client.get {
                 url(HttpRoutes.DISCOVER_MOVIES)
-                header("Authorization","Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZWVkZTk2ZDUxNTQ4NTA1ZTY3YTY4NjFhY2U3NjBmYyIsInN1YiI6IjY0YjY1MmUxMzc4MDYyMDBmZjNhNWM0NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.g3GD-e6kvUmXyPXG8EJ8lAYPBEDZQy4LQzrmn8rngBA")
+                header("Authorization", BuildConfig.AUTH_KEY)
             }
         } catch (e: Exception) {
             println("Error: ${e.message}")
