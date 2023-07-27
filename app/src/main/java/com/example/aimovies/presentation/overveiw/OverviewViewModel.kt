87de0e22f1c712f263614ff69945f86db8b041f8
AElkhami <2000088331@hexaware.com>
2023-07-27 17:03:44 +0200

    *Integrating with firebase analytics. *Sending event to firebase.

--- app/src/main/java/com/example/aimovies/presentation/overveiw/OverviewViewModel.kt
+++ app/src/main/java/com/example/aimovies/presentation/overveiw/OverviewViewModel.kt
@@ -9,6 +9,10 @@ import com.example.aimovies.domain.model.MovieModel
 import com.example.aimovies.domain.use_case.DeleteFavouriteMovie
 import com.example.aimovies.domain.use_case.GetFavouriteMovie
 import com.example.aimovies.domain.use_case.InsertFavouriteMovie
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.analytics.ktx.logEvent
+import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.launch
 
 /**
@@ -20,6 +24,8 @@ class OverviewViewModel(
     private val deleteFavouriteMovieUseCase: DeleteFavouriteMovie
 ) :
     ViewModel() {
+
+    private var firebaseAnalytics: FirebaseAnalytics = Firebase.analytics
 
     var uiState by mutableStateOf(OverviewUIModel())
         private set
@@ -39,12 +45,23 @@ class OverviewViewModel(
             insertFavouriteMovieUseCase(movie)
             uiState = uiState.copy(isMovieFavourite = true)
         }
+        movie.id?.let{
+            logAddFavouriteMovieEvent(it)
+        }
     }
 
     fun deleteFavouriteMovie(id: Long) {
         viewModelScope.launch {
             deleteFavouriteMovieUseCase(id)
             uiState = uiState.copy(isMovieFavourite = false)
+        }
+    }
+
+    fun logAddFavouriteMovieEvent(
+        movieId: Long
+    ){
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_ITEM) {
+            param(FirebaseAnalytics.Param.ITEM_ID, movieId)
         }
     }
 }
