b1368f50751a22f74d06e06b69c5ad751c00f18b
AElkhami <2000088331@hexaware.com>
2023-08-30 11:21:22 +0200

    *Restoring movie service interface as best practices.

--- app/src/main/java/com/example/aimovies/data/remote/MovieService.kt
+++ app/src/main/java/com/example/aimovies/data/remote/MovieService.kt
@@ -1,26 +1,13 @@
 package com.example.aimovies.data.remote
 
-import com.example.aimovies.BuildConfig
 import com.example.aimovies.data.remote.api_handler.Result
-import com.example.aimovies.data.remote.api_handler.handleApi
-import com.example.aimovies.data.remote.dto.DiscoverMovieResponse
-import io.ktor.client.HttpClient
-import io.ktor.client.request.get
-import io.ktor.client.request.header
-import io.ktor.client.request.parameter
-import io.ktor.client.request.url
+import com.example.aimovies.data.remote.dto.discover.DiscoverMovieResponse
+import com.example.aimovies.data.remote.dto.movie_details.MovieDetailsResponse
 
 /**
- * Created by A.Elkhami on 18/07/2023.
+ * Created by A.Elkhami on 30/08/2023.
  */
-class MovieService(private val client: HttpClient) {
-    suspend fun getDiscoverMovies(page: Int): Result<DiscoverMovieResponse> {
-        return handleApi {
-            client.get {
-                header("Authorization", BuildConfig.AUTH_KEY)
-                url(HttpRoutes.DISCOVER_MOVIES)
-                parameter("page", page)
-            }
-        }
-    }
+interface MovieService {
+    suspend fun getDiscoverMovies(page: Int): Result<DiscoverMovieResponse>
+    suspend fun getMovieById(movieId: Long): Result<MovieDetailsResponse>
 }
